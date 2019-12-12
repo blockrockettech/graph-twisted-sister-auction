a02c39cfaeb2f405017b57fec3973d0830f00613
Vincent de Almeida <vince0656@googlemail.com>
2019-12-12 11:21:23 +0000

    BidAccepted events subgraph

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,32 +1,32 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { BigInt, BigDecimal } from "@graphprotocol/graph-ts"
 import {
   Contract,
   BidAccepted,
   BidderRefunded,
   RoundFinalised
 } from "../generated/Contract/Contract"
-import { ExampleEntity } from "../generated/schema"
+import { Bid } from "../generated/schema"
+
+let ONE_ETH = new BigDecimal(BigInt.fromI32(1).times(BigInt.fromI32(10).pow(18)))
 
 export function handleBidAccepted(event: BidAccepted): void {
   // Entities can be loaded from the store using a string ID; this ID
   // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  let entity = new Bid(event.transaction.from.toHex().toString() + event.params._timeStamp.toString())
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
-  if (entity == null) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  // if (entity == null) {
+  //   entity = new Bid(event.transaction.from.toHex())
+  // }
 
   // Entity fields can be set based on event parameters
   entity._round = event.params._round
   entity._timeStamp = event.params._timeStamp
+  entity._param = event.params._param
+  entity._amount = event.params._amount
+  entity._ethAmount = new BigDecimal(event.params._amount) / ONE_ETH
+  entity._bidder = event.params._bidder
 
   // Entities can be written to the store with `.save()`
   entity.save()
